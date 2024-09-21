a3328798b22c090281e953d8de1e3150b3977975
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-09-21 11:09:24 -0300

    cache

--- obj/Debug/net8.0/HabitTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/HabitTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87825361f5b089e6c7e3f964ecc658b3dff1059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82534acdfafbc7b623042fe13eba29cde0e7310e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
